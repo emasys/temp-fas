03559bd0df7c606d9f61dc76624087051d42653d
Emmanuel Ndukwe <emasysnd@gmail.com>
2020-06-21 20:14:11 +0100

    update prod uri

--- config/index.ts
+++ config/index.ts
@@ -29,7 +29,7 @@ const config = {
   production: {
     backendURL: 'https://fas-web-api.herokuapp.com/',
     instagram: {
-      redirectUri: 'https://dc69ad3bf600.ngrok.io/auth',
+      redirectUri: 'https://fas.now.sh/auth',
       client_id: '254441985981155',
       client_secret: '9833f500a38a09a105f266ff891ecca5',
     },

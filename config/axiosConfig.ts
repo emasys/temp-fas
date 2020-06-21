d0f96aa2be80421c1013113715ba1382ab14535f
Emmanuel Ndukwe <emasysnd@gmail.com>
2020-06-21 20:27:03 +0100

    view prod logs

--- config/axiosConfig.ts
+++ config/axiosConfig.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import config from './index'
+import config from './index';
 
 export const instance = axios.create({
   baseURL: config.backendURL,
@@ -10,3 +10,6 @@ instance.defaults.headers.post['Content-Type'] =
 instance.defaults.headers.put['Content-Type'] =
   'application/x-www-form-urlencoded';
 instance.defaults.timeout = 50000;
+const data = JSON.parse(localStorage.getItem('persist:fas') || '');
+instance.defaults.headers.common['Authorization'] = data?.auth?.auth
+console.log(data.auth.auth, '====');

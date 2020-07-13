9f3d7976dfd76da2611de55df0d367feb3269bea
Emmanuel Ndukwe <emasysnd@gmail.com>
2020-07-13 15:15:36 +0100

    add sample profile page

---
+++ components/JobSearch.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import {
+  Grid,
+  FormControl,
+  FilledInput,
+  InputAdornment,
+  IconButton,
+  TextField,
+} from '@material-ui/core';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { useSelector } from 'react-redux';
+import useAutocomplete from '@material-ui/lab/useAutocomplete';
+import SelectInput from './SelectInput';
+import search from '../assets/search.svg';
+import { AppState } from '../lib/initialState';
+import { getLocations } from '../redux/selectors/locations';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    inputBox: {
+      margin: '.5rem 0',
+      backgroundColor: '#fff',
+    },
+    inputRoot: {
+      backgroundColor: '#fff',
+      width: '20rem',
+    },
+    state: {
+      width: '5.5rem',
+      [theme.breakpoints.down('sm')]: {
+        width: '5rem',
+      },
+    },
+    area: {
+      width: '5.5rem',
+      [theme.breakpoints.down('sm')]: {
+        width: '5rem',
+      },
+    },
+    formWrapper: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    icon: {
+      width: '1rem',
+    },
+  })
+);
+
+interface Props {}
+
+const JobSearch: React.FC<Props> = (props) => {
+  const classes = useStyles();
+  return (
+    <Grid item xs={12} className={classes.formWrapper}>
+      <SelectInput
+        name='state'
+        placeholder='All'
+        className={classes.state}
+        options={[]}
+        handleChange={(e) => {}}
+        value={''}
+      />
+      <SelectInput
+        name='area'
+        placeholder='This month'
+        className={classes.area}
+        options={[]}
+        handleChange={(e) => {}}
+        value={''}
+      />
+      <TextField
+        classes={{
+          root: classes.inputRoot,
+        }}
+        placeholder="Search"
+        variant='filled'
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position='end'>
+              <IconButton aria-label='search' edge='end'>
+                <img src={search} className={classes.icon} alt='search' />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+        className={classes.inputBox}
+      />
+    </Grid>
+  );
+};
+
+export default JobSearch;
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,11 +3,38 @@ import VendorLayout from '../components/VendorLayout';
 import { useSelector, useDispatch } from 'react-redux';
 import { AppState } from '../lib/initialState';
 import { handleAuthModal } from '../redux/actions/common';
-import { makeStyles, Theme, createStyles } from '@material-ui/core';
+import { makeStyles, Theme, createStyles, Grid } from '@material-ui/core';
+import JobSearch from '../components/JobSearch';
+import { fetchUserJobs } from '../redux/actions/jobs';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {},
+    search: {
+      margin: '2rem 0',
+    },
+    wrapper: {
+      minHeight: '100vh',
+      paddingTop: '5rem',
+      background:
+        'linear-gradient(90.81deg, rgba(67, 206, 162, 0.1) 0.44%, rgba(24, 90, 157, 0.1) 98.43%);',
+    },
+    row: {
+      display: 'flex',
+      alignItems: 'center',
+      height: '6rem',
+      borderRadius: '0.2rem',
+      border: '0.890421px solid #F0F0F0',
+      boxSizing: 'border-box',
+    },
+    indicator: {
+      position: 'absolute',
+      height: '3.7rem',
+      borderRadius: '0.5rem',
+      width: '0.3125rem',
+      top: '19%',
+      left: '-3px',
+    },
     blur: {
       '-webkit-filter': 'blur(1rem)',
       '-moz-filter': 'blur(1rem)',
@@ -23,19 +50,48 @@ interface Props {}
 export default function Profile({}: Props): ReactElement {
   const classes = useStyles();
   const { auth } = useSelector((state: AppState) => state.auth);
+  const jobs = useSelector((state: AppState) => state.jobs);
   const dispatch = useDispatch();
   useEffect(() => {
     if (!auth) {
       dispatch(handleAuthModal(true));
     }
+    if (auth) dispatch(fetchUserJobs());
   }, [auth]);
 
   return (
     <div className={auth ? classes.container : classes.blur}>
       <VendorLayout title={'Profile'}>
-        <div>
-          <h1>Profile</h1>
-        </div>
+        <>
+          <Grid container className={classes.search}>
+            <Grid item xs={12} sm={6}>
+              <JobSearch />
+            </Grid>
+          </Grid>
+          <Grid container className={classes.row}>
+            <Grid item xs={1} style={{ position: 'relative', height: '6rem' }}>
+              <div
+                className={classes.indicator}
+                style={{ backgroundColor: '#574497' }}
+              />
+            </Grid>
+            <Grid item xs={2}>
+              pdf
+            </Grid>
+            <Grid item xs={2}>
+              date
+            </Grid>
+            <Grid item xs={2}>
+              name
+            </Grid>
+            <Grid item xs={2}>
+              amount
+            </Grid>
+            <Grid item xs={2}>
+              status
+            </Grid>
+          </Grid>
+        </>
       </VendorLayout>
     </div>
   );

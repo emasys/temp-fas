da935d31501dcfed794e69644b2281a674e7ca37
Emmanuel Ndukwe <emasysnd@gmail.com>
2020-08-16 14:44:41 +0100

    remove vendor payment console errors

--- components/VendorPayments.tsx
+++ components/VendorPayments.tsx
@@ -1,40 +1,40 @@
-import { ReactElement, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AppState } from "../lib/initialState";
-import { getVendorStatus } from "../redux/selectors/vendors";
-import { fetchVendorPayments } from "../redux/actions/vendors";
-import PaymentRow from "./PaymentRow";
-import { makeStyles, Theme, Typography, Grid } from "@material-ui/core";
-import noResult from "../assets/no-result.svg";
+import { ReactElement, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppState } from '../lib/initialState';
+import { getVendorStatus } from '../redux/selectors/vendors';
+import { fetchVendorPayments } from '../redux/actions/vendors';
+import PaymentRow from './PaymentRow';
+import { makeStyles, Theme, Typography, Grid } from '@material-ui/core';
+import noResult from '../assets/no-result.svg';
 
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
-    padding: "3rem 0rem 3rem 3rem",
-    [theme.breakpoints.down("sm")]: {
-      padding: "2rem 0rem 2rem 1rem",
+    padding: '3rem 0rem 3rem 3rem',
+    [theme.breakpoints.down('sm')]: {
+      padding: '2rem 0rem 2rem 1rem',
     },
-    [theme.breakpoints.down("xs")]: {
-      padding: "1rem 0 1rem .5rem",
+    [theme.breakpoints.down('xs')]: {
+      padding: '1rem 0 1rem .5rem',
     },
   },
   tableHeaderContainer: {
-    textAlign: "center",
-    marginBottom: "2rem",
-    paddingLeft: "2rem",
+    textAlign: 'center',
+    marginBottom: '2rem',
+    paddingLeft: '2rem',
   },
   textHeader: {
-    marginBottom: "1rem",
-    borderBottom: "1px solid #00000038",
-    paddingBottom: ".25rem",
+    marginBottom: '1rem',
+    borderBottom: '1px solid #00000038',
+    paddingBottom: '.25rem',
     fontWeight: 100,
-    [theme.breakpoints.up("sm")]: {
-      display: "none",
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
     },
   },
   noResultContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 }));
 
@@ -52,16 +52,18 @@ export default function VendorPayments(): ReactElement {
 
   return (
     <div className={classes.container}>
-      <Typography className={classes.textHeader} variant="h5">
+      <Typography className={classes.textHeader} variant='h5'>
         Payments
       </Typography>
       <div>
         {!payments || payments?.length === 0 ? (
           <div className={classes.noResultContainer}>
-            <img src={noResult} alt="no result" />
+            <img src={noResult} alt='no result' />
           </div>
         ) : (
-          payments?.map((payment) => <PaymentRow payment={payment} />)
+          payments?.map((payment) => (
+            <PaymentRow key={payment.id} payment={payment} />
+          ))
         )}
       </div>
     </div>
